8e755eb4619903623bafeb47accd30ea1aea351e
Florian MAILLARD <florian.maillard.thebest@gmail.com>
2024-05-03 08:34:25 +0200

    Create class for handling global plot

--- plot_pomodoro_global.py
+++ plot_pomodoro_global.py
@@ -1,136 +1,140 @@
-"""Plot the global analysis of pomodoros."""
-
 import json
-from datetime import datetime
 import os
+from datetime import datetime
 import matplotlib.pyplot as plt
 import numpy as np
-import plot_pomodoro  # pylint: disable=F0401
-import pomodoro_logger  # pylint: disable=F0401
-
-DATE_PLOT = datetime.now().strftime("%y-%m-%d")
-SAVE_PNG_FOLDER = "./png_folder/"
-PLOT_NAME = SAVE_PNG_FOLDER + f"pomodoro_global_{DATE_PLOT}.png"
+from pomodoro_logger import color_title
 
 
-def charger_sessions(date):
-    """Load the pomodoros json sessions."""
-    filename = pomodoro_logger.SAVE_JSON_FOLDER + f"sessions_pomodoro_{date}.json"
-    if os.path.exists(filename):
-        with open(filename, mode="r", encoding="utf-8") as file:
-            sessions = json.load(file)
-        return sessions
+class PomodoroAnalyzer:
+    def __init__(self):
+        self.SAVE_JSON_FOLDER = "./json_folder/"
+        self.SAVE_PNG_FOLDER = "./png_folder/"
 
-    return []
+    def charger_sessions(self, date):
+        """Load the pomodoros json sessions."""
+        filename = self.SAVE_JSON_FOLDER + f"sessions_pomodoro_{date}.json"
+        if os.path.exists(filename):
+            with open(filename, mode="r", encoding="utf-8") as file:
+                sessions = json.load(file)
+            return sessions
 
+        return []
 
-def somme_pomodoros(sessions):
-    """Estimate the total kind of pomodoros."""
-    somme_realises = 0
-    somme_sous_estimation = 0
-    somme_surestimation = 0
+    @staticmethod
+    def somme_pomodoros(sessions):
+        """Estimate the total kind of pomodoros."""
+        somme_realises = 0
+        somme_sous_estimation = 0
+        somme_surestimation = 0
 
-    for session in sessions:
-        estimation_sum = int(session["Estimation 1"]) + int(session["Estimation 2"])
-        pomodoros_realises = int(session["Pomodoros effectués"])
+        for session in sessions:
+            estimation_sum = int(session["Estimation 1"]) + int(session["Estimation 2"])
+            pomodoros_realises = int(session["Pomodoros effectués"])
 
-        if pomodoros_realises < estimation_sum:
-            somme_sous_estimation += estimation_sum - pomodoros_realises
-        elif pomodoros_realises > estimation_sum:
-            somme_surestimation += pomodoros_realises - estimation_sum
+            if pomodoros_realises < estimation_sum:
+                somme_sous_estimation += estimation_sum - pomodoros_realises
+            elif pomodoros_realises > estimation_sum:
+                somme_surestimation += pomodoros_realises - estimation_sum
 
-        somme_realises += pomodoros_realises
+            somme_realises += pomodoros_realises
 
-    return somme_realises, somme_sous_estimation, somme_surestimation
+        return somme_realises, somme_sous_estimation, somme_surestimation
 
-def diff_with_first_value(arr):
-    """Compute the differences between array elements while keeping the first value intact."""
-    arr_diff = np.diff(arr)
-    return np.insert(arr_diff, 0, arr[0])
+    @staticmethod
+    def diff_with_first_value(arr):
+        """Compute the differences between array elements while keeping the first value intact."""
+        arr_diff = np.diff(arr)
+        return np.insert(arr_diff, 0, arr[0])
 
-def visualiser_sessions():
-    """Plot the graph."""
-    dates = []
-    s_realises_list = []
-    s_sous_estimation_list = []
-    s_surestimation_list = []
+    def visualiser_sessions(self, global_analysis=False):
+        """Plot the graph."""
+        if global_analysis:
+            date_plot = datetime.now().strftime("%y-%m-%d")
+            plot_name = self.SAVE_PNG_FOLDER + f"pomodoro_global_{date_plot}.png"
 
-    # Obtenez la liste des fichiers JSON dans le répertoire
-    files = [
-        file
-        for file in os.listdir(pomodoro_logger.SAVE_JSON_FOLDER)
-        if file.startswith("sessions_pomodoro_") and file.endswith(".json")
-    ]
+            dates = []
+            s_realises_list = []
+            s_sous_estimation_list = []
+            s_surestimation_list = []
 
-    for file in files:
-        # Extraire la date du nom du fichier
-        date = file.split("_")[-1].split(".")[0]
-        dates.append(date)
+            # Obtenez la liste des fichiers JSON dans le répertoire
+            files = [
+                file
+                for file in os.listdir(self.SAVE_JSON_FOLDER)
+                if file.startswith("sessions_pomodoro_") and file.endswith(".json")
+            ]
 
-        sessions = charger_sessions(date)
-        s_realises, s_sous_estimation, s_surestimation = somme_pomodoros(sessions)
-        s_realises_list.append(s_realises)
-        s_sous_estimation_list.append(s_sous_estimation)
-        s_surestimation_list.append(s_surestimation)
+            for file in files:
+                # Extraire la date du nom du fichier
+                date = file.split("_")[-1].split(".")[0]
+                dates.append(date)
 
-    plt.figure(figsize=(10, 6))
+                sessions = self.charger_sessions(date)
+                s_realises, s_sous_estimation, s_surestimation = self.somme_pomodoros(sessions)
+                s_realises_list.append(s_realises)
+                s_sous_estimation_list.append(s_sous_estimation)
+                s_surestimation_list.append(s_surestimation)
 
-    # Tracer la somme des pomodoros réalisés
-    plt.plot(
-        dates,
-        diff_with_first_value(s_realises_list),
-        marker="o",
-        color="green",
-        label="Pomodoros réalisés",
-    )
+            plt.figure(figsize=(10, 6))
 
-    # Tracer la somme des pomodoros sous-estimés
-    plt.plot(
-        dates,
-        diff_with_first_value(s_sous_estimation_list),
-        marker="o",
-        color="blue",
-        label="Sous-estimation",
-    )
-
-    # Tracer la somme des pomodoros surestimés
-    plt.plot(
-        dates,
-        diff_with_first_value(s_surestimation_list),
-        marker="o",
-        color="red",
-        label="Surestimation",
-    )
-
-    plt.xlabel("Date")
-    plt.ylabel("Somme des Pomodoros quotidiens")
-    label_list = ["Réalisés, ", "Sous-estimés, ", "Surestimés, ", "par jour"]
-    colors = ["green", "blue", "red", "black"]
-
-    plot_pomodoro.color_title(label_list, colors)
-
-    plt.grid(True)
-    plt.xticks(rotation=45)
-    plt.yticks(
-        np.arange(
-            0,
-            max(
-                *s_realises_list,
-                *s_sous_estimation_list,
-                *s_surestimation_list,
+            # Tracer la somme des pomodoros réalisés
+            plt.plot(
+                dates,
+                self.diff_with_first_value(s_realises_list),
+                marker="o",
+                color="green",
+                label="Pomodoros réalisés",
             )
-            + 1,
-        )
-    )
 
-    plt.ylim(
-        0,
-        max(*s_realises_list, *s_sous_estimation_list, *s_surestimation_list) + 1,
-    )
+            # Tracer la somme des pomodoros sous-estimés
+            plt.plot(
+                dates,
+                self.diff_with_first_value(s_sous_estimation_list),
+                marker="o",
+                color="blue",
+                label="Sous-estimation",
+            )
 
-    plt.savefig(f"{PLOT_NAME}", dpi=200)
-    plt.show()
+            # Tracer la somme des pomodoros surestimés
+            plt.plot(
+                dates,
+                self.diff_with_first_value(s_surestimation_list),
+                marker="o",
+                color="red",
+                label="Surestimation",
+            )
+
+            plt.xlabel("Date")
+            plt.ylabel("Somme des Pomodoros quotidiens")
+            label_list = ["Réalisés, ", "Sous-estimés, ", "Surestimés, ", "par jour"]
+            colors = ["green", "blue", "red", "black"]
+
+            color_title(label_list, colors)
+
+            plt.grid(True)
+            plt.xticks(rotation=45)
+            plt.yticks(
+                np.arange(
+                    0,
+                    max(
+                        *s_realises_list,
+                        *s_sous_estimation_list,
+                        *s_surestimation_list,
+                    )
+                    + 1,
+                )
+            )
+
+            plt.ylim(
+                0,
+                max(*s_realises_list, *s_sous_estimation_list, *s_surestimation_list) + 1,
+            )
+
+            plt.savefig(plot_name, dpi=200)
+            plt.show()
 
 
 if __name__ == "__main__":
-    visualiser_sessions()
+    analyzer = PomodoroAnalyzer()
+    analyzer.visualiser_sessions(global_analysis=True)
