41dee443fd9989474b0fb65cdf25ef887f514097
Florian MAILLARD <florian.maillard.thebest@gmail.com>
2024-05-03 08:13:00 +0200

    Class creation
    the class upport the plotting

--- pomodoro_logger.py
+++ pomodoro_logger.py
@@ -3,54 +3,219 @@
 import json
 import os
 from datetime import datetime
-
-DATE = datetime.now().strftime("%y-%m-%d")
-SAVE_JSON_FOLDER = "./json_folder/"
-JSON_NAME = SAVE_JSON_FOLDER + f"sessions_pomodoro_{DATE}.json"
+import matplotlib.pyplot as plt
+import numpy as np
 
 
-def enregistrer_session(tache, duree, estimation_initiale, estimation_finale):
-    """Register the session pomodoro."""
-    session = {
-        "Tâche": tache,
-        "Pomodoros effectués": duree,
-        "Estimation 1": estimation_initiale,
-        "Estimation 2": estimation_finale,
-    }
-    if not os.path.exists(JSON_NAME):
-        with open(JSON_NAME, mode="w", encoding="utf-8") as file1:
-            json.dump([session], file1, ensure_ascii=False, indent=4)
-    else:
-        with open(JSON_NAME, mode="r+", encoding="utf-8") as file2:
+def color_title(labels, colors, textprops=None, a_x=None, y_0=1.013):
+    """
+    Create a good colorfull title.
+
+    Creates a centered title with multiple colors. Don't change axes limits
+    afterwards.
+    """
+    precision = 10**-2
+    if textprops is None:
+        textprops = {"size": "large"}
+    if a_x is None:
+        a_x = plt.gca()
+
+    plt.gcf().canvas.draw()
+    transform = a_x.transAxes  # use axes coords
+
+    # initial params
+    x_t = 0  # where the text ends in x-axis coords
+    shift = 0  # where the text starts
+
+    # for text objects
+    text = {}
+
+    while (np.abs(shift - (1 - x_t)) > precision) and (shift <= x_t):
+        x_pos = shift
+
+        for label, col in zip(labels, colors):
             try:
-                data = json.load(file2)
-            except json.decoder.JSONDecodeError:
-                data = []
-            data.append(session)
-            file2.seek(0)
-            json.dump(data, file2, ensure_ascii=False, indent=4)
+                text[label].remove()
+            except KeyError:
+                pass
 
+            text[label] = a_x.text(
+                x_pos,
+                y_0,
+                label,
+                transform=transform,
+                ha="left",
+                color=col,
+                **textprops,
+            )
 
-def main():
-    """Compute the main part of the script."""
-    print("Bienvenue dans PomodoroTracker !")
-    while True:
-        tache = input("Nom de la tâche : ")
-        duree = input("Nombre de Pomodoros effectués : ")
-        estimation_initiale = input("1ère estimation (Pomodoros) : ")
-        estimation_finale = input("2ème estimation (Pomodoros) : ")
+            x_pos = text[label].get_window_extent().transformed(transform.inverted()).x1
 
-        enregistrer_session(tache, duree, estimation_initiale, estimation_finale)
+        x_t = x_pos  # where all text ends
 
-        continuer = (
-            input("Voulez-vous enregistrer une autre session ? ([oui]/non) : ") or "oui"
-        )
-        if continuer.lower() == "non" or continuer[0].lower() == "n":
+        shift += precision / 2  # increase for next iteration
+
+        if x_pos > 1:  # guardrail
             break
 
 
+def get_date():
+    """Demander à l'utilisateur de choisir une date ou laisser vide pour aujourd'hui."""
+    date_input = input("Veuillez entrer une date au format yy-mm-dd (laissez vide pour aujourd'hui) : ")
+    if not date_input:
+        return datetime.now().strftime("%y-%m-%d")
+    else:
+        try:
+            datetime.strptime(date_input, "%y-%m-%d")
+            return date_input
+        except ValueError:
+            print("Format de date incorrect. Utilisez le format yy-mm-dd.")
+            return get_date()
+
+class PomodoroTracker:
+    def __init__(self, date):
+        self.SAVE_JSON_FOLDER = "./json_folder/"
+        self.SAVE_PNG_FOLDER = './png_folder/'
+        self.DATE = date
+        self.PLOT_NAME = self.SAVE_PNG_FOLDER + f"pomodoro_{self.DATE}.png"
+
+    def enregistrer_session(self, tache, duree, estimation_initiale, estimation_finale):
+        """Register the session pomodoro."""
+        json_name = self.SAVE_JSON_FOLDER + f"sessions_pomodoro_{self.DATE}.json"
+        session = {
+            "Tâche": tache,
+            "Pomodoros effectués": duree,
+            "Estimation 1": estimation_initiale,
+            "Estimation 2": estimation_finale,
+        }
+        if not os.path.exists(json_name):
+            with open(json_name, mode="w", encoding="utf-8") as file:
+                json.dump([session], file, ensure_ascii=False, indent=4)
+        else:
+            with open(json_name, mode="r+", encoding="utf-8") as file:
+                try:
+                    data = json.load(file)
+                except json.decoder.JSONDecodeError:
+                    data = []
+                data.append(session)
+                file.seek(0)
+                json.dump(data, file, ensure_ascii=False, indent=4)
+
+    def visualiser_sessions(self, bshow):
+        """Plot the graph."""
+        json_name = self.SAVE_JSON_FOLDER + f"sessions_pomodoro_{self.DATE}.json"
+        if not os.path.exists(json_name):
+            print("Aucune session disponible pour cette date.")
+            return
+
+        with open(json_name, mode="r", encoding="utf-8") as file:
+            sessions = json.load(file)
+
+        pomodoros_realises = []
+        surestimation = []
+        sous_estimation = []
+        pomodoros_effectues = []
+        taches = []
+
+        for session in sessions:
+            estimation_sum = int(session["Estimation 1"]) + int(session["Estimation 2"])
+            pomodoros_realises.append(int(session["Pomodoros effectués"]))
+            pomodoros_effectues.append(len(pomodoros_effectues) + 1)
+            taches.append(session["Tâche"])
+
+            if pomodoros_realises[-1] < estimation_sum:
+                surestimation.append(estimation_sum)
+                sous_estimation.append(np.nan)  # Utiliser NaN pour les valeurs manquantes
+            elif pomodoros_realises[-1] > estimation_sum:
+                sous_estimation.append(estimation_sum)
+                surestimation.append(np.nan)  # Utiliser NaN pour les valeurs manquantes
+            else:
+                surestimation.append(np.nan)
+                sous_estimation.append(np.nan)
+
+        plt.figure(figsize=(10, 6))
+
+        # Tracer la ligne pour le nombre de pomodoros réalisés
+        plt.plot(
+            pomodoros_effectues,
+            pomodoros_realises,
+            marker="o",
+            color="green",
+            label="Pomodoros réalisés",
+        )
+
+        # Tracer la ligne pour la surestimation
+        plt.plot(
+            pomodoros_effectues,
+            surestimation,
+            marker="o",
+            color="red",
+            label="Surestimation",
+        )
+
+        # Tracer la ligne pour la sous-estimation
+        plt.plot(
+            pomodoros_effectues,
+            sous_estimation,
+            marker="o",
+            color="blue",
+            label="Sous-estimation",
+        )
+
+        plt.xlabel("Session Pomodoro")
+        plt.ylabel("Nombre de Pomodoros")
+
+        label_list = ["Réalisés, ", "Sous-estimés, ", "Surestimés, ", f"{self.DATE}"]
+        colors = ["green", "blue", "red", "black"]
+
+        color_title(label_list, colors)
+
+        # Ajouter les noms de tâche associés à l'axe des abscisses
+        plt.xticks(pomodoros_effectues, taches, rotation=45, ha="right")
+        # Ajuster les ticks et l'échelle des ordonnées pour les pomodoros entiers
+        plt.yticks(
+            np.arange(
+                0,
+                max(*pomodoros_realises, *surestimation, *sous_estimation) + 1,
+                1,
+            )
+        )
+
+        # Réglez les limites de l'axe des ordonnées
+        plt.ylim(0, max(*pomodoros_realises, *surestimation, *sous_estimation) + 1)
+
+        plt.grid(True)
+        plt.savefig(self.PLOT_NAME, dpi=200)
+        if bshow:
+            plt.show()
+
+    def main(self):
+        """Compute the main part of the script."""
+        while True:
+            tache = input("Nom de la tâche : ")
+            duree = input("Nombre de Pomodoros effectués : ")
+            estimation_initiale = input("1ère estimation (Pomodoros) : ")
+            estimation_finale = input("2ème estimation (Pomodoros) : ")
+
+            self.enregistrer_session(tache, duree, estimation_initiale, estimation_finale)
+
+            continuer = (
+                    input("Voulez-vous enregistrer une autre session ? ([oui]/non) : ") or "oui"
+            )
+            if continuer.lower() == "non" or continuer[0].lower() == "n":
+                break
+
+
 if __name__ == "__main__":
-    if not os.path.exists(JSON_NAME):
-        with open(JSON_NAME, mode="w", encoding="utf-8") as file:
-            pass
-    main()
+    print("Bienvenue dans PomodoroTracker !")
+    while True:
+        date = get_date()
+        pomodoro_tracker = PomodoroTracker(date)
+        pomodoro_tracker.main()
+        tracer_session = input("Voulez-vous tracer la figure de cette journée ? (oui/[non]) : ") or "non"
+        if tracer_session.lower() == "oui" or tracer_session[0].lower() == "o":
+            pomodoro_tracker.visualiser_sessions(False)
+        continuer_global = input(
+            "Voulez-vous enregistrer une session pour une autre date ? ([oui]/non) : ") or "oui"
+        if continuer_global.lower() == "non" or continuer_global[0].lower() == "n":
+            break
